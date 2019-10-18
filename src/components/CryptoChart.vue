e5c375359dad80ccaaf1a9de73ff2ba24bd94ac6
denkoroku <cathriddoch@gmail.com>
2019-10-18 15:00:44 +0100

    first commit

---
+++ src/components/CryptoChart.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+  <GChart
+    v-if="chartData"
+    type="ColumnChart"
+    :data="chartData"
+    :options="chartOptions"
+  />
+</template>
+
+<script>
+import { GChart } from 'vue-google-charts'
+export default {
+  name: 'crypto-chart',
+  data(){
+    return {
+      chartOptions: {
+        width: 800,
+        height: 240,
+        title: 'Top 10 cryptocurrencies',
+      }
+    }
+  },
+  props: ['cryptoData'],
+  components : {
+    GChart
+  },
+  computed: {
+    chartData: function(){
+      if(this.chartData){
+        const chartData = this.cryptoData.map((currencyObject)=>{
+          return [currencyObject.name, currencyObject.current_price]
+        }
+        return chartData
+      }
+      return null
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/CryptoSelector.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <h1>{{cryptocurrency}}</h1>
+</template>
+
+<script>
+export default {
+  name:"crypto-selector",
+  props:["cryptocurrency"]
+};
+</script>
+
+<style lang="css" scoped>
+</style>
